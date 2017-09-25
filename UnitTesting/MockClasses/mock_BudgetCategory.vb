1a01e575847624c0f37c21ebb250faed600a0d15
Andrew Starr <astarr@appraisalresearch.cc>
2017-09-25 16:34:41 -0400

    Added mock_BudgetCategory to UnitTesting project

---
+++ UnitTesting/MockClasses/mock_BudgetCategory.vb
@@ -0,0 +1,15 @@
+﻿Imports Expense_Parser
+
+Public Class mock_BudgetCategory
+    Implements IBudgetCategory
+
+    Public Property Budget As Double Implements IBudgetCategory.Budget
+
+    Public Property Name As String Implements IBudgetCategory.Name
+
+    Public Property Payees As List(Of String) Implements IBudgetCategory.Payees
+
+    Public Property Type As BudgetTypes Implements IBudgetCategory.Type
+
+    Public Property Used As Boolean Implements IBudgetCategory.Used
+End Class
