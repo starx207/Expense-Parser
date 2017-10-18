a7f4a044f076c799fba6b2555aa86a62a7dc9700
Andrew Starr <astarr@appraisalresearch.cc>
2017-10-18 12:45:42 -0400

    Adding ExcelExporter Tests

---
+++ UnitTesting/cls_ExcelExporter_Test.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports Expense_Parser
+Imports System.Diagnostics.CodeAnalysis
+
+<ExcludeFromCodeCoverage>
+<TestClass()> Public Class cls_ExcelExporter_Test
+
+    <TestMethod()>
+    Public Sub ExcelExporter_ClassExists()
+        Dim textExport As Object
+        Try
+            textExport = New ExcelExporter
+        Catch ex As Exception
+            Assert.Fail("Class ""ExcelExporter"" not implemented")
+        End Try
+
+        ' Test passes if it gets here
+        Assert.IsTrue(True)
+    End Sub
+
+End Class
